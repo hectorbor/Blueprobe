4018055ea7ea3070c7d5fadb48e0831852c0c400
Héctor Borreguero Monleón <hectorbor03@gmail.com>
2024-12-19 20:13:43 -0300

    ADD: important changes in the code of the GUI: columns system for switching between different windows, choice of bluetooth classic or BLE, buttons for date and general formatting of the main screen.

--- Programa/GUI.py
+++ Programa/GUI.py
@@ -9,20 +9,31 @@
 import PySimpleGUI as sg
 import csv
 
-# All the stuff inside your window.
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.FileBrowse(key='-FILE_PATH-')],            
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+# Creating layouts for every possible window
+layout1 = [ [sg.Text('Introduce the .csv file to be analyzed'), sg.FileBrowse(key='-FILE_PATH-')],
+            [sg.CalendarButton("From", close_when_date_chosen=True,  target='-DEPARTURE-', location=(0,0), no_titlebar=False ), sg.Input(key='-DEPARTURE-', size=(20,1))],
+            [sg.CalendarButton("To", close_when_date_chosen=True,  target='-ARRIVAL-', location=(0,0), no_titlebar=False ), sg.Input(key='-ARRIVAL-', size=(20,1))],
+            [sg.Radio('Bluetooth Classic', group_id=1, default=True), sg.Radio('Bluetooth Low Energy', group_id=1)],            
+            [sg.Button('Analyze'), sg.Button('Clear')] ]
+
+layout2 = [ [sg.Text('Successful change')]]
+
+# Organizing all the layouts
+layout = [      [sg.Column(layout1, key='COL1'),
+     sg.Column(layout2, key='COL2', visible=False)] ]
 
 # Create the Window
 window = sg.Window('Window Title', layout)
 
+col1, col2 = window['COL1'], window['COL2']
+
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+    if event == sg.WIN_CLOSED : # if user closes window or clicks cancel
         break
-    print('You entered ', values[0])
+    elif event == 'Analyze' :
+        col2.update(visible=True)
+        col1.update(visible=False)
 
 window.close()
