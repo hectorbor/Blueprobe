ba2a75b5323b68217ff0b296c89f7d5a9c8373fc
Héctor Borreguero Monleón <hectorbor03@gmail.com>
2024-12-22 23:50:41 -0300

    ADD: added a way to know all the possible sources and destinations that have happened in the connection and style changes

--- Programa/GUI.py
+++ Programa/GUI.py
@@ -33,23 +33,37 @@ def filter_by_time(start_date,end_date, values):
         else:
             print('\n'+'incorrecto'+'fecha inicio: '+start_date.strftime("%m/%d/%Y, %H:%M:%S")+' fecha de final: '+end_date.strftime("%m/%d/%Y, %H:%M:%S")+' fecha de prueba: '+elem_date.strftime("%m/%d/%Y, %H:%M:%S"))
     return res
+
+def find_sources_and_destinations(values_timed):
+    res=[[],[]]
+    for i in values_timed:
+        print('importante '+i['source'])
+        print('importante '+i['destination'])
+        if not i['source'] in res[0]:
+            res[0].append(i['source'])
+        if not  i['destination'] in res[1]:
+            res[1].append(i['destination'])
+    return res
+
     
 
 # Creating layouts for every possible screen
-layout1 = [ [sg.Text('Introduce the .csv file to be analyzed'), sg.FileBrowse(key='-FILE_PATH-')],
+layout1 = [ [sg.Text('Introduce the .csv file to be analyzed', background_color='#0082FC'), sg.FileBrowse(key='-FILE_PATH-',button_color=('#FFFFFF','#0A3C91'))],
             [sg.CalendarButton("From", close_when_date_chosen=True,  target='-FROM-', location=(0,0), no_titlebar=False ), sg.Input(key='-FROM-', size=(20,1))],
             [sg.CalendarButton("To", close_when_date_chosen=True,  target='-TO-', location=(0,0), no_titlebar=False ), sg.Input(key='-TO-', size=(20,1))],
             [sg.Radio('Bluetooth Classic', group_id=1, default=True), sg.Radio('Bluetooth Low Energy', group_id=1)],            
             [sg.Button('Analyze'), sg.Button('Clear')] ]
 
-layout2 = [ [sg.Text('Successful change')]]
+layout2 = [ [sg.Text('Successful analysis.')],
+            [sg.Text('Sources', background_color='#0082FC'),sg.Combo([''], key='-SOURCES-',size=((20,3)))],
+            [sg.Text('Destinations', background_color='#0082FC'),sg.Combo([''], key='-DESTINATIONS-',size=((20,3)))]]
 
 # Organizing all the layouts
-layout = [  [sg.Column(layout1, key='COL1'),
-            sg.Column(layout2, key='COL2', visible=False)] ]
+layout = [  [sg.Column(layout1, key='COL1', background_color='#0082FC'),
+            sg.Column(layout2, key='COL2', background_color='#0082FC',visible=False),sg.Sizegrip(background_color='#0082FC')] ]
 
 # Create the Window
-window = sg.Window('Window Title', layout)
+window = sg.Window('Window Title', layout, resizable=True, background_color='#0082FC')
 
 col1, col2 = window['COL1'], window['COL2']
 
@@ -64,17 +78,26 @@ while True:
         print(csv_values[0])
         dict_values=[]
         for i in csv_values:
-            dict_element={"order":i[0],"time":i[1].split('.')[0],"source":i[2],"destination":i[3], "type":i[4], "code":i[5], "description":i[6]}
+            dict_element={"order":i[0],"time":i[1].split('.')[0],"source":i[2],"destination":i[3], "type":i[4], "code":i[5], "description":i[6], "OpCode":i[7], "OpGroup":i[8], "Bytes":i[9]}
             dict_values.append(dict_element)
         datetime_from = datetime.strptime(values["-FROM-"], '%Y-%m-%d %H:%M:%S')
         datetime_to = datetime.strptime(values["-TO-"], '%Y-%m-%d %H:%M:%S')
         datetime_test = datetime.strptime(dict_values[0]["time"], '%Y-%m-%d %H:%M:%S')
         dict_values_timed=filter_by_time(datetime_from,datetime_to,dict_values)
+        srcs_and_dests=find_sources_and_destinations(dict_values_timed)
+        sources_element = window.find_element('-SOURCES-', silent_on_error=True)
+        destinations_element = window.find_element('-DESTINATIONS-', silent_on_error=True)  
+        sources_element.update(values=srcs_and_dests[0])
+        destinations_element.update(values=srcs_and_dests[1])
 
-        print(dict_values)
-        print(datetime_test)
+        #print(dict_values)
+        #print(datetime_test)
         print("\n \n")
         print(dict_values_timed)
+
+        print('qpasó 0'+str(srcs_and_dests))
+        print('qpasó 1'+str(srcs_and_dests[0]))
+        print('qpasó 2'+str(srcs_and_dests[1]))
 
         col2.update(visible=True)
         col1.update(visible=False)
